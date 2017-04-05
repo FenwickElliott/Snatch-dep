0500863b3cdbe988fb017e2ab8e199467f4a2abd
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2017-04-05 09:50:36 -0400

    snatch controller generated

---
+++ app/controllers/snatch_controller.rb
@@ -0,0 +1,10 @@
+class SnatchController < ApplicationController
+  def signup
+  end
+
+  def snatch
+  end
+
+  def about
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'snatch/signup'
+
+  get 'snatch/snatch'
+
+  get 'snatch/about'
+
+  root 'snatch#snatch'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
