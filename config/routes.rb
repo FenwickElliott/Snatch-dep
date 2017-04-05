fbb28f50c00738e99c023711e7497968085734e8
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2017-04-05 12:26:00 -0400

    bootstrap incorporated

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
 
   get 'snatch/about'
 
+  get 'about' => 'snatch#about'
+  get 'signup' => 'snatch#signup'
+
   root 'snatch#snatch'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
