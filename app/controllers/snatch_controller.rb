9ddf0b249ea4e972a69a4ede21f008a07bbb7697
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2017-04-06 12:43:47 -0400

    RestClient, JSON.parse and api call functioning

--- app/controllers/snatch_controller.rb
+++ app/controllers/snatch_controller.rb
@@ -1,9 +1,11 @@
 class SnatchController < ApplicationController
+  require 'rest-client'
+  require 'JSON'
   def signup
   end
 
   def snatch
-
+    @rc = JSON.parse RestClient.get "https://api.spotify.com/v1/tracks/6NPVjNh8Jhru9xOmyQigds"
   end
 
   def about
